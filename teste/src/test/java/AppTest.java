e1346bae245f3a496b94a7fce33b6e09fdb55bab
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2023-11-25 17:21:26 -0300

    Refactor: created a method to insert a new egg

--- teste/src/test/java/AppTest.java
+++ teste/src/test/java/AppTest.java
@@ -1,3 +1,4 @@
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -5,26 +6,58 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public class AppTest {
+    String url = "D:\\Faculdade\\Códigos\\Visual Studio\\Site a ser testado - TC1\\eggcellent-management-interface\\msedgedriver.exe";
+    String pageIndex = "D:\\Faculdade\\Códigos\\Visual Studio\\Site a ser testado - TC1\\eggcellent-management-interface\\pages\\index.html";
+    WebDriver driver = new EdgeDriver();
+
 
     @Test
-    @DisplayName("should go to add new egg page")
-    void shouldGoToAddNewEggPage() {
-        String url = "D:\\Faculdade\\Códigos\\Visual Studio\\Site a ser testado - TC1\\eggcellent-management-interface\\msedgedriver.exe";
-        String pageIndex = "D:\\Faculdade\\Códigos\\Visual Studio\\Site a ser testado - TC1\\eggcellent-management-interface\\pages\\index.html";
+    @DisplayName("should register  a new egg")
+    void shouldRegisterANewEgg() {
+        addNewEgg();
+    }
 
+    private void addNewEgg() {
         System.setProperty("webdriver.edge.driver", url);
+        driver.get(pageIndex);
 
-        WebDriver driver = new EdgeDriver();
+        WebElement addNewEggButton = driver.findElement(By.linkText("Add New Egg"));
+        addNewEggButton.click();
 
-        try {
-            driver.get(pageIndex);
+        WebElement name = driver.findElement(By.id("name"));
+        WebElement birthday = driver.findElement(By.id("birthday"));
+        List<WebElement> languages = driver.findElements(By.className("form-check-input"));
+        WebElement firstParent = driver.findElement(By.id("parentSelect"));
+        WebElement secondParent = driver.findElement(By.id("secondParentSelect"));
 
-            WebElement addNewEggButton = driver.findElement(By.linkText("Add New Egg"));
-            addNewEggButton.click();
+        Faker faker = new Faker();
 
-        } finally {
-            driver.quit();
+        name.sendKeys(faker.name().fullName());
+
+        String pattern = "dd/MM/yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        birthday.sendKeys(simpleDateFormat.format(faker.date().past(5, TimeUnit.DAYS)));
+
+        int alteracoes = (int) Math.floor(Math.random() * languages.size());
+        for (int i = 0; i < alteracoes; i++) {
+            languages.get((int) Math.floor(Math.random() * languages.size())).click();
         }
+
+        List<String> firstParentOptions = firstParent.findElements(By.tagName("option")).stream().map(option -> option.getAttribute("value")).toList();
+        firstParent.sendKeys(firstParentOptions.get((int) Math.floor(Math.random() * firstParentOptions.size())));
+
+        List<String> secondParentOptions = secondParent.findElements(By.tagName("option")).stream().map(option -> option.getAttribute("value")).toList();
+        secondParent.sendKeys(secondParentOptions.get((int) Math.floor(Math.random() * secondParentOptions.size())));
+
+        WebElement registerButton = driver.findElement(By.xpath("//button[text()='Register']"));
+        registerButton.click();
+
+        WebElement returnIndex = driver.findElement(By.linkText("Back to Index"));
+        returnIndex.click();
     }
 }
