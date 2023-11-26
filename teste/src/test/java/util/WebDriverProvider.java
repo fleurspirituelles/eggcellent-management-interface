58cf2608f3526a7390710fc0da8c201bfee41f42
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-11-26 19:28:11 -0300

    refactor: turned WebDriver in a utility class

--- teste/src/test/java/util/WebDriverProvider.java
+++ teste/src/test/java/util/WebDriverProvider.java
@@ -1,7 +1,32 @@
 package util;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
-public interface WebDriverProvider {
-    WebDriver getByBrowserName(String browserName);
+public final class WebDriverProvider {
+    public static WebDriver getByBrowserName(String browserName) {
+        return switch (browserName) {
+            case "chrome" -> {
+                WebDriverManager.chromedriver().setup();
+                yield new ChromeDriver();
+            }
+            case "edge" -> {
+                WebDriverManager.edgedriver().setup();
+                yield new EdgeDriver();
+            }
+            case "firefox" -> {
+                WebDriverManager.firefoxdriver().setup();
+                yield new FirefoxDriver();
+            }
+            case "safari" -> {
+                WebDriverManager.safaridriver().setup();
+                yield new SafariDriver();
+            }
+            default -> throw new IllegalArgumentException("Invalid browser name. Provided: " + browserName);
+        };
+    }
 }
