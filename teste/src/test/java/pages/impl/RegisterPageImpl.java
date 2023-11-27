073a6109e8ebab72421ef9051b138ff80d6dd077
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2023-11-27 11:35:48 -0300

    style: remove unused imports

--- teste/src/test/java/pages/impl/RegisterPageImpl.java
+++ teste/src/test/java/pages/impl/RegisterPageImpl.java
@@ -7,13 +7,10 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.IndexPage;
 import pages.RegisterPage;
-import util.Utilities;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.time.Duration;
 import java.util.Objects;
-import java.util.Properties;
+
 
 import static util.Utilities.getPagesDirectory;
 
