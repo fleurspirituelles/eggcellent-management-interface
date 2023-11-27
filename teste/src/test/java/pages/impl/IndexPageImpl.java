9accd675a8cfb93da0b5b98c118d79790d374e62
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-11-27 17:44:43 -0300

    fix: fixed eggs table rows selection issue

--- teste/src/test/java/pages/impl/IndexPageImpl.java
+++ teste/src/test/java/pages/impl/IndexPageImpl.java
@@ -2,76 +2,71 @@ package pages.impl;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import pages.*;
-import util.Utilities;
+import pages.EditPage;
+import pages.EggComponent;
+import pages.IndexPage;
+import pages.RegisterPage;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
 import java.util.Objects;
-import java.util.Properties;
 
 import static util.Utilities.getPagesDirectory;
 
 public final class IndexPageImpl implements IndexPage {
     private final WebDriver driver;
-    private final List<EggComponentImpl> eggs;
 
-    private IndexPageImpl(WebDriver driver, List<EggComponentImpl> eggs) {
+    private IndexPageImpl(WebDriver driver) {
         this.driver = driver;
-        this.eggs = eggs;
     }
 
     public static IndexPage openPage(WebDriver driver) {
         Objects.requireNonNull(driver, "The web driver cannot be null!");
 
         driver.get("file:"+getPageURI());
-        var eggs = findEggs(driver);
+        var index = new IndexPageImpl(driver);
+        index.waitPageLoad();
 
-        return new IndexPageImpl(driver, eggs);
+        return index;
     }
 
     public static IndexPage movingTo(WebDriver driver) {
-        var eggs = findEggs(driver);
-        return new IndexPageImpl(driver, eggs);
+        var index = new IndexPageImpl(driver);
+        index.waitPageLoad();
+        return index;
     }
 
     private static String getPageURI() {
         return getPagesDirectory() + "/index.html";
     }
 
-    private static List<EggComponentImpl> findEggs(WebDriver driver) {
-        wainPage(driver);
-        return driver.findElements(By.tagName("tr"))
-                .stream()
-                .map(EggComponentImpl::new)
-                .toList();
-    }
-
-    private static void wainPage(WebDriver driver) {
-        new WebDriverWait(driver, Duration.ofSeconds(3))
+    @Override
+    public void waitPageLoad() {
+        new WebDriverWait(driver, Duration.ofSeconds(5))
                 .until(ExpectedConditions.presenceOfElementLocated(By.id("tableBody")));
     }
 
     @Override
-    public void waitPageLoad() {
-        wainPage(driver);
-    }
-
-    @Override
     public int getNumberOfEggs() {
-        return eggs.size();
+        return getEggsElements().size();
     }
 
     @Override
     public EggComponent getEggByIndex(int index) {
         if (index < 0)
             throw new IllegalArgumentException("The index must be a non-negative number! Provided: " + index);
-        return eggs.get(index);
+        return getEggsElements().stream()
+                .map(EggComponentImpl::new)
+                .toList()
+                .get(index);
+    }
+
+    private List<WebElement> getEggsElements() {
+        return new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("#tableBody tr")));
     }
 
     @Override
@@ -81,7 +76,7 @@ public final class IndexPageImpl implements IndexPage {
 
     @Override
     public void deleteLast() {
-        deleteEggByIndex(eggs.size() - 1);
+        deleteEggByIndex(getNumberOfEggs() - 1);
     }
 
     @Override
