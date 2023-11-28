6f2e50adf4793d797571ff747c7e76bc676b335a
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-11-28 13:56:47 -0300

    refactor: replaced pages-config.properties by tests.config.properties and improved Utilities class

--- teste/src/test/java/pages/impl/util/Utilities.java
+++ teste/src/test/java/pages/impl/util/Utilities.java
@@ -1,29 +1,35 @@
 package pages.impl.util;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public final class Utilities {
-    private static final String PAGES_CONFIG_PROPERTIES_PATH = System.getProperty("user.dir") +
-            "/src/test/resources/pages/impl/pages-config.properties";
+    private static final String PAGES_CONFIG_PROPERTIES_PATH = "tests.config.properties";
 
     public static String getPagesDirectory() {
-        var properties = new Properties();
+        var directory = getPropertiesResource().getProperty("pages.directory.name");
 
-        try (var stream = new FileInputStream(PAGES_CONFIG_PROPERTIES_PATH)) {
+        if (directory == null) throw new MissingConfigurationException(
+            "It is missing pages.directory.name property which should detail the absolute path to the pages folder."
+        );
+
+        return directory;
+    }
+
+    private static Properties getPropertiesResource() {
+        var properties = new Properties();
+        var loader = Utilities.class.getClassLoader();
+
+        try (var stream = loader.getResourceAsStream(PAGES_CONFIG_PROPERTIES_PATH)) {
             properties.load(stream);
         }
         catch (IOException e) {
-            throw new MissingConfigurationException("It is missing pages-config file detailing pages.directory.name " +
-                    "prop which is intended to be use to facilitate tests configuration.");
+            throw new MissingConfigurationException(
+                "It is missing tests.config.properties files at resources folder detailing required configurations " +
+                    "to correctly find the pages folder. Please create file: src/test/resources/tests.config.properties"
+            );
         }
 
-        var directory = properties.getProperty("pages.directory.name");
-        if (directory == null)
-            throw new MissingConfigurationException("It is missing pages.directory.name property detailing the " +
-                    "absolute path to the pages folder");
-
-        return directory;
+        return properties;
     }
 }
